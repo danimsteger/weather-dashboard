71a3285727707fea48bc250b2f0cf3e6473744c2
danimsteger <daniellestegertutoring@gmail.com>
2024-06-24 16:01:44 -0400

    began working on api request functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 const searchFormEl = $("#search-form");
 const searchInputEl = $("#search-input");
+const currentWeatherEl = $("#current-weather");
 
 function handleSearchFormSubmit(event) {
   event.preventDefault();
@@ -7,15 +8,49 @@ function handleSearchFormSubmit(event) {
   const searchInputVal = searchInputEl.val();
   console.log(searchInputVal);
 
-  if (!searchInputVal) {
-    console.error("You need a search input value!");
-    return;
-  }
+  //   if (!searchInputVal) {
+  //     console.error("You need a search input value!");
+  //     return;
+  //   }
 
-  const queryString = `api.openweathermap.org/data/2.5/forecast?q=${searchInputVal}&appid=4f82dd0d149294627ba2d28ac435f1d2
-`;
+  //   const queryString = `api.openweathermap.org/data/2.5/forecast?q=${searchInputVal}&appid=4f82dd0d149294627ba2d28ac435f1d2
+  // `;
 
-  console.log(queryString);
+  //   console.log(queryString);
 }
 
+// function searchApi() {
+//   const queryURL =
+//     "api.openweathermap.org/data/2.5/forecast?q=${searchInputVal}&appid=4f82dd0d149294627ba2d28ac435f1d2";
+// }
+
+const getCityWeather = function (city) {
+  const apiUrl =
+    "api.openweathermap.org/data/2.5/forecast?q=${city}&appid=4f82dd0d149294627ba2d28ac435f1d2";
+
+  fetch(apiUrl)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          displayCityWeather(data, city);
+        });
+      } else {
+        alert("Error:${response.statusText}");
+      }
+    })
+    .catch(function (error) {
+      alert("Unable to connect to OpenWeather");
+    });
+};
+
+// const displayWeather = function (data, searchTerm) {
+//   if (MediaCapabilities.length === 0) {
+//     currentWeatherEl.textContent = "City not found.";
+//     return;
+//   }
+
+//   for (let cityObj of city) {
+//     const cityName =
+//   }
+// };
 searchFormEl.on("submit", handleSearchFormSubmit);
